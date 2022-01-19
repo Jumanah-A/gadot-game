ef26ed2cd4f9423572c4eef3dd90a3df90932739
Fawaz <falharbi.m.96@gmail.com>
2022-01-19 10:39:47 -0800

    add basic player movement attack, and enemy mechanics

---
+++ Versions/INF151/Characters/Character.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+class_name Character
+
+const FRICTION: float = 0.15
+
+export(int) var hp: int = 3
+export(int) var acceleration: int = 55
+export(int) var max_speed: int = 90
+
+onready var state_machine: Node = get_node("FiniteStateMachine")
+onready var animated_sprite: AnimatedSprite = get_node("AnimatedSprite")
+
+var mov_direction: Vector2 = Vector2.ZERO
+var velocity: Vector2 = Vector2.ZERO
+
+func _physics_process(_delta: float) -> void:
+	velocity = move_and_slide(velocity)
+	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
+
+func move() -> void:
+	mov_direction = mov_direction.normalized()
+	velocity += mov_direction * acceleration
+	velocity = velocity.clamped(max_speed)
+
+
+func take_damage(dam: int, dir: Vector2, force: int) -> void:
+	hp -= dam
+	print(hp)
+	if hp > 0:
+		state_machine.set_state(state_machine.states.hurt)
+		velocity += dir * force
+	else:
+		state_machine.set_state(state_machine.states.dead)
+		velocity += dir * force * 2
