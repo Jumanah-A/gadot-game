2a0a26a180a0c6e095fef1072b7ce4dc6db696c1
Fawaz <falharbi.m.96@gmail.com>
2022-02-13 12:16:27 -0800

    start enemy edits

---
+++ Versions/INF151/RadialSpawner.gd
@@ -0,0 +1,16 @@
+extends "res://Spawner.gd"
+
+export var spawning_radius = 100.0
+
+func _ready():
+	spawn()
+
+func spawn():
+	var spawnling = .spawn()
+	
+	var radial_offset = Vector2.RIGHT.rotated(rand_range(0.0, TAU))
+	radial_offset *= rand_range(0.0, spawning_radius)
+	
+	spawnling.global_position += radial_offset
+	return spawnling
+	
