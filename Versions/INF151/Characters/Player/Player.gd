be012f4b58240394f6efcf8ca7b5644ce3a2341c
Zach1281 <zachary_romero@yahoo.com>
2022-02-20 15:35:20 -0800

    aspect ratio clamping

--- Versions/INF151/Characters/Player/Player.gd
+++ Versions/INF151/Characters/Player/Player.gd
@@ -33,8 +33,8 @@ func _process(_delta: float) -> void:
 	
 #	restrict charcter to screen size
 	position += velocity * _delta
-	position.x = clamp(position.x, 0, get_viewport_rect().size.x)
-	position.y = clamp(position.y, 0, get_viewport_rect().size.y)
+	#position.x = clamp(position.x, 0, get_viewport_rect().size.x)
+	#position.y = clamp(position.y, 0, get_viewport_rect().size.y)
 
 
 func move() -> void:
